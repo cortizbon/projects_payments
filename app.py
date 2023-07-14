9e3e62a52fa73e5934cda17642060fb65657fe65
Carlos Ortiz <carlosortizbon@gmail.com>
2023-07-13 21:07:35 -0500

    functions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import boto3
 import pandas as pd
+from utils import client, return_df
 
 st.title("Payment info download")
 
@@ -8,40 +9,20 @@ st.write("Write the nickname in your email. If your mail is pepo90@lmail.com, wr
 
 var_name = st.text_input("Write the nickname in your email. If your mail is pepo90@lmail.com, write 'pepo90', no spaces.")
 
-s3 = boto3.client('s3', 
-                  region_name='us-east-1', 
-                  aws_access_key_id=st.secrets['one'], 
-                  aws_secret_access_key=st.secrets['two'])
+try:
+    client(var_name)
+    csv1, csv2 = return_df(var_name)
 
-bucket_name = 'bucket-csv-files'
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.download_button("Download detailed projects info here.",
+                        csv1,
+                        f"{var_name}_projects.csv")
 
-response = s3.list_objects_v2(Bucket=bucket_name)
-
-s3.download_file(Bucket=bucket_name,
-    Key=f"pay_info/{var_name}/grouped.csv",
-    Filename=f"{var_name}_grouped.csv")
-
-s3.download_file(Bucket=bucket_name,
-    Key=f"pay_info/{var_name}/projects.csv",
-    Filename=f"{var_name}_projects.csv")
-
-col1, col2 = st.columns(2)
-
-df1 = pd.read_csv(f"{var_name}_projects.csv")
-df2 = pd.read_csv(f"{var_name}_grouped.csv")
-
-def convert_df(df):
-   return df.to_csv(index=False).encode('utf-8')
-
-csv1 = convert_df(df1)
-csv2 = convert_df(df2)
-
-with col1:
-    st.download_button("Download detailed projects info here.",
-                       csv1,
-                       f"{var_name}_projects.csv")
-
-with col2:
-    st.download_button("Download grouped info here.",
-                       csv2,
-                       f"{var_name}_grouped.csv")
+    with col2:
+        st.download_button("Download grouped info here.",
+                        csv2,
+                        f"{var_name}_grouped.csv")
+except:
+    st.warning("Either your nickname is not correct or you have not written any name.")
---
+++ utils.py
@@ -0,0 +1,29 @@
+import boto3
+import pandas as pd
+def client(var_name):
+    s3 = boto3.client('s3', 
+                        region_name='us-east-1', 
+                        aws_access_key_id=st.secrets['one'], 
+                        aws_secret_access_key=st.secrets['two'])
+
+    bucket_name = 'bucket-csv-files'
+
+    response = s3.list_objects_v2(Bucket=bucket_name)
+
+    s3.download_file(Bucket=bucket_name,
+            Key=f"pay_info/{var_name}/grouped.csv",
+            Filename=f"{var_name}_grouped.csv")
+
+    s3.download_file(Bucket=bucket_name,
+            Key=f"pay_info/{var_name}/projects.csv",
+            Filename=f"{var_name}_projects.csv")
+    
+    df1 = pd.read_csv(f"{var_name}_projects.csv")
+    df2 = pd.read_csv(f"{var_name}_grouped.csv")
+
+def return_df(var_name):
+    df1 = pd.read_csv(f"{var_name}_projects.csv")
+    df2 = pd.read_csv(f"{var_name}_grouped.csv")
+    csv1 = df1.to_csv(df1, index=False).enconde('utf-8')
+    csv2 = df2.to_csv(df1, index=False).enconde('utf-8') 
+    return csv1, csv2
