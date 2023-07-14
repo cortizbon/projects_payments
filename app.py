4d89eae131eae8444ad0c47d8f49cb6855365742
Carlos Ortiz <carlosortizbon@gmail.com>
2023-07-13 21:09:14 -0500

    exception

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import boto3
+from botocore.exceptions import ClientError
 import pandas as pd
 from utils import client, return_df
 
 st.title("Payment info download")
 
-st.write("Write the nickname in your email. If your mail is pepo90@lmail.com, write 'pepo90', no spaces.")
+
 
 var_name = st.text_input("Write the nickname in your email. If your mail is pepo90@lmail.com, write 'pepo90', no spaces.")
 
@@ -14,7 +15,7 @@ try:
     csv1, csv2 = return_df(var_name)
 
     col1, col2 = st.columns(2)
-    
+
     with col1:
         st.download_button("Download detailed projects info here.",
                         csv1,
@@ -24,5 +25,5 @@ try:
         st.download_button("Download grouped info here.",
                         csv2,
                         f"{var_name}_grouped.csv")
-except:
+except ClientError:
     st.warning("Either your nickname is not correct or you have not written any name.")
